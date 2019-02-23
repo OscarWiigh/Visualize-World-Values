2c88f15430d83401cdcbe05ea5a8d6613f69b5d9
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-02-23 12:29:23 +0100

    Updates

---
+++ precache-manifest.3d5085ad25b7161b210be57c4a734f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd915444fbd4ac0bc451",
+    "url": "/Visualize-World-Values/static/js/runtime~main.cd915444.js"
+  },
+  {
+    "revision": "f4d84c5009c9a93919f7",
+    "url": "/Visualize-World-Values/static/js/main.f4d84c50.chunk.js"
+  },
+  {
+    "revision": "0537012a48183eb40c7a",
+    "url": "/Visualize-World-Values/static/js/1.0537012a.chunk.js"
+  },
+  {
+    "revision": "f4d84c5009c9a93919f7",
+    "url": "/Visualize-World-Values/static/css/main.394132e5.chunk.css"
+  },
+  {
+    "revision": "6fbdab15bf8411ed4959caae8f74e2df",
+    "url": "/Visualize-World-Values/index.html"
+  }
+];
